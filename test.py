303d2015bef766de59b0c73b317cd6790c7390c9
Yash1ro <LewisGuyh@outlook.com>
2023-04-10 14:06:31 +0800

    change 1

--- test.py
+++ test.py
@@ -11,31 +11,31 @@ print(img)
 transform = torchvision.transforms.Compose(
     [torchvision.transforms.Resize((32, 32)),
      torchvision.transforms.ToTensor(),
-    ]
+     ]
 )
 
 img = transform(img)
 print(img.shape)
 
 
-# class Tangyan(nn.Module):
-#     def __init__(self):
-#         super(Tangyan, self).__init__()
-#         self.model1 = Sequential(
-#             Conv2d(3, 32, 5, padding=2),
-#             MaxPool2d(2),
-#             Conv2d(32, 32, 5, padding=2),
-#             MaxPool2d(2),
-#             Conv2d(32, 64, 5, padding=2),
-#             MaxPool2d(2),
-#             Flatten(),
-#             Linear(1024, 64),
-#             Linear(64, 10)
-#         )
-#
-#     def forward(self, x):
-#         x = self.model1(x)
-#         return x
+class Tangyan(nn.Module):
+    def __init__(self):
+        super(Tangyan, self).__init__()
+        self.model1 = Sequential(
+            Conv2d(3, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 32, 5, padding=2),
+            MaxPool2d(2),
+            Conv2d(32, 64, 5, padding=2),
+            MaxPool2d(2),
+            Flatten(),
+            Linear(1024, 64),
+            Linear(64, 10)
+        )
+
+    def forward(self, x):
+        x = self.model1(x)
+        return x
 
 
 model = torch.load("tangyan_3.pth")
@@ -47,4 +47,3 @@ with torch.no_grad():
 print(output)
 
 print(output.argmax(1))
-
